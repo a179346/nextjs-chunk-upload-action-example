10915aaefa7cbd21608ef9bb247b4f9fa9ba1a21
a179346 <r122164152@gmail.com>
2024-02-28 22:09:30 +0800

    add readme & UploadForm

--- src/app/components/upload-form.tsx
+++ src/app/components/upload-form.tsx
@@ -1,16 +1,3 @@
-## nextjs-chunk-upload-action-demo
-demo [nextjs-chunk-upload-action](https://github.com/a179346/nextjs-chunk-upload-action)
-
-## Start
-```sh
-npm run dev
-```
-
-## Usage
-
-```ts
-// upload-form.tsx
-
 "use client";
 
 import { ChunkUploader } from "nextjs-chunk-upload-action";
@@ -57,38 +44,3 @@ export function UploadForm() {
     </form>
   );
 }
-```
-
-```ts
-// chunk-upload-action.ts
-
-"use server";
-
-import { createWriteStream } from "fs";
-import { join } from "path";
-
-export async function chunkUploadAction(
-  base64Chunk: string,
-  offset: number,
-  metadata: { name: string }
-) {
-  const buffer = Buffer.from(base64Chunk, "base64");
-  const path = join("./uploads", metadata.name);
-
-  const writeable = createWriteStream(
-    path,
-    offset === 0
-      ? { flags: "w" }
-      : {
-          flags: "r+",
-          start: offset,
-        }
-  );
-  return new Promise<void>((resolve, reject) => {
-    writeable.on("finish", () => resolve());
-    writeable.on("error", reject);
-    writeable.write(buffer);
-    writeable.end();
-  });
-}
-```
---
+++ src/server/chunk-upload-action.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { createWriteStream } from "fs";
+import { join } from "path";
+
+export async function chunkUploadAction(
+  base64Chunk: string,
+  offset: number,
+  metadata: { name: string }
+) {
+  const buffer = Buffer.from(base64Chunk, "base64");
+  const path = join("./uploads", metadata.name);
+
+  const writeable = createWriteStream(
+    path,
+    offset === 0
+      ? { flags: "w" }
+      : {
+          flags: "r+",
+          start: offset,
+        }
+  );
+  return new Promise<void>((resolve, reject) => {
+    writeable.on("finish", () => resolve());
+    writeable.on("error", reject);
+    writeable.write(buffer);
+    writeable.end();
+  });
+}
